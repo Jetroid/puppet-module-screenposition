f0b4e232ddf922ec02e284cd8485d1a0aa47b2d4
Jetroid <holtjet@gmail.com>
2015-09-14 13:13:43 +0100

    Update Fact standards

--- lib/facter/xrandr_output.rb
+++ lib/facter/xrandr_output.rb
@@ -1,8 +1,8 @@
-# xrandr.rb
+# xrandr_output.rb
 # Fact to get the output of xrandr to find out the monitor details
 
     Facter.add('xrandr_output') do
       setcode do
-        Facter::Core::Execution.exec('DISPLAY=:0 xrandr -q --verbose')
+        Facter::Util::Resolution.exec('DISPLAY=:0 xrandr -q --verbose')
       end
     end
